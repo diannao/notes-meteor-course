aa5e58762fda2e3c4c0b2cb00ab9ea50d4ed44dc
Steve Barr <steve.barr@pobox.com>
2017-05-28 16:47:15 -0400

    Integrate selectedNoteId into list items and url

--- client/main.js
+++ client/main.js
@@ -3,9 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
+import createBrowserHistory from 'history/createBrowserHistory'
 
 import { routes, onAuthChange } from '../imports/routes/routes';
 import '../imports/startup/simple-schema-configuration.js';
+
+const history = createBrowserHistory({ forceRefresh: true }); //
+
+//window.browserHistory = history;
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
@@ -13,7 +18,19 @@ Tracker.autorun(() => {
   onAuthChange(isAuthenticated);
 });
 
+Tracker.autorun(() => {
+  const selectedNoteId = Session.get('selectedNoteId');
+
+  if(selectedNoteId) {
+        history.replace(`/dashboard/${selectedNoteId}`);
+  }
+});
+
+
 Meteor.startup(() => {
+  Session.set('selectedNoteId', undefined);
+
+
   //  Meteor.call('links.insert', 'fail');
     ReactDOM.render(routes, document.getElementById('app'));
 });
--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -5,23 +5,39 @@ import { mount } from 'enzyme';
 
 import NoteListItem from './NoteListItem';
 
+import { notes } from '../fixtures/fixtures';
+
 if(Meteor.isClient) {
   describe('NoteListItem', function() {
-    it('should render title and timestamp', function() {
-      const title = 'My title here';
-      const updatedAt = 1486137505429;
-      const wrapper = mount(<NoteListItem note={{title, updatedAt}}/>);
 
-      expect(wrapper.find('h5').text()).toBe(title);
+    let Session;
+
+    beforeEach(() => {
+      Session = {
+        set: expect.createSpy()
+      };
+    });
+
+    it('should render title and timestamp', function() {
+
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
+
+      expect(wrapper.find('h5').text()).toBe(notes[0].title);
       expect(wrapper.find('p').text()).toBe('2/03/17');
     });
 
     it('should set default title if no title passed in', function() {
-      const title = '';
-      const updatedAt = 1486137505429;
-      const wrapper = mount(<NoteListItem note={{title, updatedAt}}/>);
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
 
       expect(wrapper.find('h5').text()).toBe('Untitled note');
+    });
+
+    it('should call set on click', function() {
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
+
+      wrapper.find('div').simulate('click');
+
+//      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
     });
   });
 }
